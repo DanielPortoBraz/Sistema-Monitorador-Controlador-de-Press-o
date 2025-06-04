a3cfef43e9013a08d391841694445d5f291ca968
DanielPortoBraz <danielpb132@gmail.com>
2025-06-04 18:43:47 -0300

    Arquivos do Projeto

---
+++ Pressure_Control.c
@@ -0,0 +1,525 @@
+ /**
+  * Autor: Daniel Porto Braz
+  * 
+  * 
+  * 
+  * Este código é baseado naquele do repositório de Ricardo Prattes: https://github.com/rmprates84/mqtt_client
+ */
+
+#include "pico/stdlib.h"            // Biblioteca da Raspberry Pi Pico para funções padrão (GPIO, temporização, etc.)
+#include "pico/cyw43_arch.h"        // Biblioteca para arquitetura Wi-Fi da Pico com CYW43
+#include "pico/unique_id.h"         // Biblioteca com recursos para trabalhar com os pinos GPIO do Raspberry Pi Pico
+
+#include "hardware/gpio.h"          // Biblioteca de hardware de GPIO
+#include "hardware/irq.h"           // Biblioteca de hardware de interrupções
+#include "hardware/adc.h"           // Biblioteca de hardware para conversão ADC
+#include "hardware/pwm.h"           // Biblioteca de hardware de PWM
+
+#include "lwip/apps/mqtt.h"         // Biblioteca LWIP MQTT -  fornece funções e recursos para conexão MQTT
+#include "lwip/apps/mqtt_priv.h"    // Biblioteca que fornece funções e recursos para Geração de Conexões
+#include "lwip/dns.h"               // Biblioteca que fornece funções e recursos suporte DNS:
+#include "lwip/altcp_tls.h"         // Biblioteca que fornece funções e recursos para conexões seguras usando TLS:
+
+#define WIFI_SSID "wifi"                  // Substitua pelo nome da sua rede Wi-Fi
+#define WIFI_PASSWORD "senha_wifi"      // Substitua pela senha da sua rede Wi-Fi
+#define MQTT_SERVER "endereco_host"                // Substitua pelo endereço do host - broket MQTT: Ex: 192.168.1.107
+#define MQTT_USERNAME "usuario"     // Substitua pelo nome da host MQTT - Username
+#define MQTT_PASSWORD "senha_usuario" // Substitua pelo Password da host MQTT - credencial de acesso - caso exista
+
+
+#ifndef MQTT_SERVER
+#error Need to define MQTT_SERVER
+#endif
+
+// This file includes your client certificate for client server authentication
+#ifdef MQTT_CERT_INC
+#include MQTT_CERT_INC
+#endif
+
+#ifndef MQTT_TOPIC_LEN
+#define MQTT_TOPIC_LEN 100
+#endif
+
+//Dados do cliente MQTT
+typedef struct {
+    mqtt_client_t* mqtt_client_inst;
+    struct mqtt_connect_client_info_t mqtt_client_info;
+    char data[MQTT_OUTPUT_RINGBUF_SIZE];
+    char topic[MQTT_TOPIC_LEN];
+    uint32_t len;
+    ip_addr_t mqtt_server_address;
+    bool connect_done;
+    int subscribe_count;
+    bool stop_client;
+} MQTT_CLIENT_DATA_T;
+
+#ifndef DEBUG_printf
+#ifndef NDEBUG
+#define DEBUG_printf printf
+#else
+#define DEBUG_printf(...)
+#endif
+#endif
+
+#ifndef INFO_printf
+#define INFO_printf printf
+#endif
+
+#ifndef ERROR_printf
+#define ERROR_printf printf
+#endif
+
+// Manter o programa ativo - keep alive in seconds
+#define MQTT_KEEP_ALIVE_S 60
+
+// QoS - mqtt_subscribe
+// At most once (QoS 0)
+// At least once (QoS 1)
+// Exactly once (QoS 2)
+#define MQTT_SUBSCRIBE_QOS 1
+#define MQTT_PUBLISH_QOS 1
+#define MQTT_PUBLISH_RETAIN 0
+
+// Tópico usado para: last will and testament
+#define MQTT_WILL_TOPIC "/online"
+#define MQTT_WILL_MSG "0"
+#define MQTT_WILL_QOS 1
+
+#ifndef MQTT_DEVICE_NAME
+#define MQTT_DEVICE_NAME "pico"
+#endif
+
+// Definir como 1 para adicionar o nome do cliente aos tópicos, para suportar vários dispositivos que utilizam o mesmo servidor
+#ifndef MQTT_UNIQUE_TOPIC
+#define MQTT_UNIQUE_TOPIC 0
+#endif
+
+// Pinos 
+// Joystick
+const uint8_t VRy = 26;
+const uint8_t ADC_CHAN_0 = 0;
+static uint16_t vry_value;
+
+// Matriz de LEDs
+#define MATRIX_PIN 7
+
+// LED RGB
+#define LED_RED_PIN 13
+#define LED_GREEN_PIN 11
+#define LED_BLUE_PIN 12
+
+// Buzzer
+const uint8_t BUZZER_PIN = 21;
+const uint16_t PERIOD = 17750; // WRAP
+const float DIVCLK = 16.0; // Divisor inteiro
+static uint slice_21;
+const uint16_t dc_values[] = {PERIOD * 0.3, 0}; // Duty Cycle de 30% e 0%
+static volatile uint16_t dc_level = PERIOD * 0.3; // Nível do Duty Cycle
+static volatile bool toogle = false;
+
+// ------ INICIALIZAÇÕES ----------
+
+// Inicializa o joystick e os conversores ADC
+void init_joystick(){
+    adc_init();
+    adc_gpio_init(VRy);
+}
+
+// Obtém o valor atual das coordenadas do joystick com base na leitura ADC 
+void read_joystick(){
+    adc_select_input(ADC_CHAN_0);
+    sleep_us(2);
+    vry_value = adc_read();
+}
+
+
+// Inicialização do LED RGB
+void init_led(){
+    gpio_init(LED_RED_PIN);
+    gpio_set_dir(LED_RED_PIN, GPIO_OUT);
+    gpio_put(LED_RED_PIN, 0);
+    
+    gpio_init(LED_GREEN_PIN);
+    gpio_set_dir(LED_GREEN_PIN, GPIO_OUT);
+    gpio_put(LED_GREEN_PIN, 0);
+    
+    gpio_init(LED_BLUE_PIN);
+    gpio_set_dir(LED_BLUE_PIN, GPIO_OUT);
+    gpio_put(LED_BLUE_PIN, 0);
+}
+
+// Configuração do buzzer em PWM
+void setup_pwm(){
+
+    // PWM do BUZZER
+    // Configura para soar 440 Hz
+    gpio_set_function(BUZZER_PIN, GPIO_FUNC_PWM);
+    slice_21 = pwm_gpio_to_slice_num(BUZZER_PIN);
+    pwm_set_clkdiv(slice_21, DIVCLK);
+    pwm_set_wrap(slice_21, PERIOD);
+    pwm_set_gpio_level(BUZZER_PIN, 0);
+    pwm_set_enabled(slice_21, true);
+}
+
+
+// --------- FUNÇÕES --------
+
+// Converte o valor adc do eixo x do joystick para porcentagem entre 0 e 100%
+float conversion_level_pressure(){
+    return (vry_value * 100) / 4095;
+}
+
+// Requisição para publicar
+static void pub_request_cb(__unused void *arg, err_t err);
+
+// Topico MQTT
+static const char *full_topic(MQTT_CLIENT_DATA_T *state, const char *name);
+
+// Controle do LED 
+static void control_rgb_led(MQTT_CLIENT_DATA_T *state, int color);
+
+// Requisição de Assinatura - subscribe
+static void sub_request_cb(void *arg, err_t err);
+
+// Requisição para encerrar a assinatura
+static void unsub_request_cb(void *arg, err_t err);
+
+// Tópicos de assinatura
+static void sub_unsub_topics(MQTT_CLIENT_DATA_T* state, bool sub);
+
+// Dados de entrada MQTT
+static void mqtt_incoming_data_cb(void *arg, const u8_t *data, u16_t len, u8_t flags);
+
+// Dados de entrada publicados
+static void mqtt_incoming_publish_cb(void *arg, const char *topic, u32_t tot_len);
+
+// Publicar pressão
+static void pressure_worker_fn(async_context_t *context, async_at_time_worker_t *worker);
+static async_at_time_worker_t pressure_worker = { .do_work = pressure_worker_fn };
+
+// Conexão MQTT
+static void mqtt_connection_cb(mqtt_client_t *client, void *arg, mqtt_connection_status_t status);
+
+// Inicializar o cliente MQTT
+static void start_client(MQTT_CLIENT_DATA_T *state);
+
+// Call back com o resultado do DNS
+static void dns_found(const char *hostname, const ip_addr_t *ipaddr, void *arg);
+
+
+// ------ PROGRAMA PRINCIPAL ---------
+int main(void) {
+
+    // Inicializa todos os tipos de bibliotecas stdio padrão presentes que estão ligados ao binário.
+    stdio_init_all();
+    INFO_printf("mqtt client starting\n");
+
+    init_joystick();
+
+    init_led();
+
+    setup_pwm();
+
+    // Cria registro com os dados do cliente
+    static MQTT_CLIENT_DATA_T state;
+
+    // Inicializa a arquitetura do cyw43
+    if (cyw43_arch_init()) {
+        panic("Failed to inizialize CYW43");
+    }
+
+    // Usa identificador único da placa
+    char unique_id_buf[5];
+    pico_get_unique_board_id_string(unique_id_buf, sizeof(unique_id_buf));
+    for(int i=0; i < sizeof(unique_id_buf) - 1; i++) {
+        unique_id_buf[i] = tolower(unique_id_buf[i]);
+    }
+
+    // Gera nome único, Ex: pico1234
+    char client_id_buf[sizeof(MQTT_DEVICE_NAME) + sizeof(unique_id_buf) - 1];
+    memcpy(&client_id_buf[0], MQTT_DEVICE_NAME, sizeof(MQTT_DEVICE_NAME) - 1);
+    memcpy(&client_id_buf[sizeof(MQTT_DEVICE_NAME) - 1], unique_id_buf, sizeof(unique_id_buf) - 1);
+    client_id_buf[sizeof(client_id_buf) - 1] = 0;
+    INFO_printf("Device name %s\n", client_id_buf);
+
+    state.mqtt_client_info.client_id = client_id_buf;
+    state.mqtt_client_info.keep_alive = MQTT_KEEP_ALIVE_S; // Keep alive in sec
+#if defined(MQTT_USERNAME) && defined(MQTT_PASSWORD)
+    state.mqtt_client_info.client_user = MQTT_USERNAME;
+    state.mqtt_client_info.client_pass = MQTT_PASSWORD;
+#else
+    state.mqtt_client_info.client_user = NULL;
+    state.mqtt_client_info.client_pass = NULL;
+#endif
+    static char will_topic[MQTT_TOPIC_LEN];
+    strncpy(will_topic, full_topic(&state, MQTT_WILL_TOPIC), sizeof(will_topic));
+    state.mqtt_client_info.will_topic = will_topic;
+    state.mqtt_client_info.will_msg = MQTT_WILL_MSG;
+    state.mqtt_client_info.will_qos = MQTT_WILL_QOS;
+    state.mqtt_client_info.will_retain = true;
+#if LWIP_ALTCP && LWIP_ALTCP_TLS
+    // TLS enabled
+#ifdef MQTT_CERT_INC
+    static const uint8_t ca_cert[] = TLS_ROOT_CERT;
+    static const uint8_t client_key[] = TLS_CLIENT_KEY;
+    static const uint8_t client_cert[] = TLS_CLIENT_CERT;
+    // This confirms the indentity of the server and the client
+    state.mqtt_client_info.tls_config = altcp_tls_create_config_client_2wayauth(ca_cert, sizeof(ca_cert),
+            client_key, sizeof(client_key), NULL, 0, client_cert, sizeof(client_cert));
+#if ALTCP_MBEDTLS_AUTHMODE != MBEDTLS_SSL_VERIFY_REQUIRED
+    WARN_printf("Warning: tls without verification is insecure\n");
+#endif
+#else
+    state->client_info.tls_config = altcp_tls_create_config_client(NULL, 0);
+    WARN_printf("Warning: tls without a certificate is insecure\n");
+#endif
+#endif
+
+    // Conectar à rede WiFI - fazer um loop até que esteja conectado
+    cyw43_arch_enable_sta_mode();
+    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+        panic("Failed to connect");
+    }
+    INFO_printf("\nConnected to Wifi\n");
+
+    //Faz um pedido de DNS para o endereço IP do servidor MQTT
+    cyw43_arch_lwip_begin();
+    int err = dns_gethostbyname(MQTT_SERVER, &state.mqtt_server_address, dns_found, &state);
+    cyw43_arch_lwip_end();
+
+    // Se tiver o endereço, inicia o cliente
+    if (err == ERR_OK) {
+        start_client(&state);
+    } else if (err != ERR_INPROGRESS) { // ERR_INPROGRESS means expect a callback
+        panic("dns request failed");
+    }
+
+    // Loop condicionado a conexão mqtt
+    while (!state.connect_done || mqtt_client_is_connected(state.mqtt_client_inst)) {
+        cyw43_arch_poll();
+        cyw43_arch_wait_for_work_until(make_timeout_time_ms(10000));
+    }
+
+    INFO_printf("mqtt client exiting\n");
+    return 0;
+}
+
+
+// Requisição para publicar
+static void pub_request_cb(__unused void *arg, err_t err) {
+    if (err != 0) {
+        ERROR_printf("pub_request_cb failed %d", err);
+    }
+}
+
+//Topico MQTT
+static const char *full_topic(MQTT_CLIENT_DATA_T *state, const char *name) {
+#if MQTT_UNIQUE_TOPIC
+    static char full_topic[MQTT_TOPIC_LEN];
+    snprintf(full_topic, sizeof(full_topic), "/%s%s", state->mqtt_client_info.client_id, name);
+    return full_topic;
+#else
+    return name;
+#endif
+}
+
+// Controle do LED RGB
+static void control_rgb_led(MQTT_CLIENT_DATA_T *state, int color) {
+    char* message;
+
+    // Emite um beep para indicar mudança de cor do LED
+    pwm_set_gpio_level(BUZZER_PIN, dc_values[0]);
+    sleep_ms(200);
+    pwm_set_gpio_level(BUZZER_PIN, dc_values[1]);
+
+    if (color == 0){
+        strcpy(message, "green");
+        gpio_put(LED_RED_PIN, 0);
+        gpio_put(LED_BLUE_PIN, 0);
+        gpio_put(LED_GREEN_PIN, 1);
+
+    } else if(color == 1){
+        strcpy(message, "yellow");
+        gpio_put(LED_BLUE_PIN, 0);
+        gpio_put(LED_GREEN_PIN, 1);
+        gpio_put(LED_RED_PIN, 1);
+
+    } else if(color == 2){
+        strcpy(message, "red");
+        gpio_put(LED_GREEN_PIN, 0);
+        gpio_put(LED_BLUE_PIN, 0);
+        gpio_put(LED_RED_PIN, 1);
+    } else{
+        strcpy(message, "off");
+        gpio_put(LED_GREEN_PIN, 0);
+        gpio_put(LED_BLUE_PIN, 0);
+        gpio_put(LED_RED_PIN, 0);
+    }
+
+    mqtt_publish(state->mqtt_client_inst, full_topic(state, "/rgb_led/color"), message, strlen(message), MQTT_PUBLISH_QOS, MQTT_PUBLISH_RETAIN, pub_request_cb, state);
+}
+
+
+// Publicar pressão
+static void publish_pressure(MQTT_CLIENT_DATA_T *state){
+    read_joystick();
+    static float old_pressure;
+    const char *pressure_key = full_topic(state, "/pressure");
+    float pressure = conversion_level_pressure();
+    if (pressure != old_pressure) {
+        old_pressure = pressure;
+        // Publicar a pressão no tópico /pressure
+        char press_str[16];
+        snprintf(press_str, sizeof(press_str), "%.2f", pressure);
+        INFO_printf("Publishing %s to %s\n", press_str, pressure_key);
+        mqtt_publish(state->mqtt_client_inst, pressure_key, press_str, strlen(press_str), MQTT_PUBLISH_QOS, MQTT_PUBLISH_RETAIN, pub_request_cb, state);
+    }
+    
+}
+
+// Requisição de Assinatura - subscribe
+static void sub_request_cb(void *arg, err_t err) {
+    MQTT_CLIENT_DATA_T* state = (MQTT_CLIENT_DATA_T*)arg;
+    if (err != 0) {
+        panic("subscribe request failed %d", err);
+    }
+    state->subscribe_count++;
+}
+
+// Requisição para encerrar a assinatura
+static void unsub_request_cb(void *arg, err_t err) {
+    MQTT_CLIENT_DATA_T* state = (MQTT_CLIENT_DATA_T*)arg;
+    if (err != 0) {
+        panic("unsubscribe request failed %d", err);
+    }
+    state->subscribe_count--;
+    assert(state->subscribe_count >= 0);
+
+    // Stop if requested
+    if (state->subscribe_count <= 0 && state->stop_client) {
+        mqtt_disconnect(state->mqtt_client_inst);
+    }
+}
+
+// Tópicos de assinatura
+static void sub_unsub_topics(MQTT_CLIENT_DATA_T* state, bool sub) {
+    mqtt_request_cb_t cb = sub ? sub_request_cb : unsub_request_cb;
+    mqtt_sub_unsub(state->mqtt_client_inst, full_topic(state, "/rgb_led"), MQTT_SUBSCRIBE_QOS, cb, state, sub);
+    mqtt_sub_unsub(state->mqtt_client_inst, full_topic(state, "/print"), MQTT_SUBSCRIBE_QOS, cb, state, sub);
+    mqtt_sub_unsub(state->mqtt_client_inst, full_topic(state, "/ping"), MQTT_SUBSCRIBE_QOS, cb, state, sub);
+    mqtt_sub_unsub(state->mqtt_client_inst, full_topic(state, "/exit"), MQTT_SUBSCRIBE_QOS, cb, state, sub);
+}
+
+// Dados de entrada MQTT
+static void mqtt_incoming_data_cb(void *arg, const u8_t *data, u16_t len, u8_t flags) {
+    MQTT_CLIENT_DATA_T* state = (MQTT_CLIENT_DATA_T*)arg;
+#if MQTT_UNIQUE_TOPIC
+    const char *basic_topic = state->topic + strlen(state->mqtt_client_info.client_id) + 1;
+#else
+    const char *basic_topic = state->topic;
+#endif
+    strncpy(state->data, (const char *)data, len);
+    state->len = len;
+    state->data[len] = '\0';
+
+    DEBUG_printf("Topic: %s, Message: %s\n", state->topic, state->data);
+    if (strcmp(basic_topic, "/rgb_led") == 0)
+    {
+        if (lwip_stricmp((const char *)state->data, "green") == 0)
+            control_rgb_led(state, 0);
+        else if (lwip_stricmp((const char *)state->data, "yellow") == 0)
+            control_rgb_led(state, 1);
+        else if (lwip_stricmp((const char *)state->data, "red") == 0)
+            control_rgb_led(state, 2);
+        else if (lwip_stricmp((const char *)state->data, "off") == 0)
+            control_rgb_led(state, 3);
+
+    } else if (strcmp(basic_topic, "/print") == 0) {
+        INFO_printf("%.*s\n", len, data);
+    } else if (strcmp(basic_topic, "/ping") == 0) {
+        char buf[11];
+        snprintf(buf, sizeof(buf), "%u", to_ms_since_boot(get_absolute_time()) / 1000);
+        mqtt_publish(state->mqtt_client_inst, full_topic(state, "/uptime"), buf, strlen(buf), MQTT_PUBLISH_QOS, MQTT_PUBLISH_RETAIN, pub_request_cb, state);
+    } else if (strcmp(basic_topic, "/exit") == 0) {
+        state->stop_client = true; // stop the client when ALL subscriptions are stopped
+        sub_unsub_topics(state, false); // unsubscribe
+    }
+}
+
+// Dados de entrada publicados
+static void mqtt_incoming_publish_cb(void *arg, const char *topic, u32_t tot_len) {
+    MQTT_CLIENT_DATA_T* state = (MQTT_CLIENT_DATA_T*)arg;
+    strncpy(state->topic, topic, sizeof(state->topic));
+}
+
+// Publicar pressão
+static void pressure_worker_fn(async_context_t *context, async_at_time_worker_t *worker) {
+    MQTT_CLIENT_DATA_T* state = (MQTT_CLIENT_DATA_T*)worker->user_data;
+    publish_pressure(state);
+    async_context_add_at_time_worker_in_ms(context, worker, 500);
+}
+
+// Conexão MQTT
+static void mqtt_connection_cb(mqtt_client_t *client, void *arg, mqtt_connection_status_t status) {
+    MQTT_CLIENT_DATA_T* state = (MQTT_CLIENT_DATA_T*)arg;
+    if (status == MQTT_CONNECT_ACCEPTED) {
+        state->connect_done = true;
+        sub_unsub_topics(state, true); // subscribe;
+
+        // indicate online
+        if (state->mqtt_client_info.will_topic) {
+            mqtt_publish(state->mqtt_client_inst, state->mqtt_client_info.will_topic, "1", 1, MQTT_WILL_QOS, true, pub_request_cb, state);
+        }
+
+        // Publica a pressão a cada 500 ms
+        pressure_worker.user_data = state;
+        async_context_add_at_time_worker_in_ms(cyw43_arch_async_context(), &pressure_worker, 0);
+    } else if (status == MQTT_CONNECT_DISCONNECTED) {
+        if (!state->connect_done) {
+            panic("Failed to connect to mqtt server");
+        }
+    }
+    else {
+        panic("Unexpected status");
+    }
+}
+
+// Inicializar o cliente MQTT
+static void start_client(MQTT_CLIENT_DATA_T *state) {
+#if LWIP_ALTCP && LWIP_ALTCP_TLS
+    const int port = MQTT_TLS_PORT;
+    INFO_printf("Using TLS\n");
+#else
+    const int port = MQTT_PORT;
+    INFO_printf("Warning: Not using TLS\n");
+#endif
+
+    state->mqtt_client_inst = mqtt_client_new();
+    if (!state->mqtt_client_inst) {
+        panic("MQTT client instance creation error");
+    }
+    INFO_printf("IP address of this device %s\n", ipaddr_ntoa(&(netif_list->ip_addr)));
+    INFO_printf("Connecting to mqtt server at %s\n", ipaddr_ntoa(&state->mqtt_server_address));
+
+    cyw43_arch_lwip_begin();
+    if (mqtt_client_connect(state->mqtt_client_inst, &state->mqtt_server_address, port, mqtt_connection_cb, state, &state->mqtt_client_info) != ERR_OK) {
+        panic("MQTT broker connection error");
+    }
+#if LWIP_ALTCP && LWIP_ALTCP_TLS
+    // This is important for MBEDTLS_SSL_SERVER_NAME_INDICATION
+    mbedtls_ssl_set_hostname(altcp_tls_context(state->mqtt_client_inst->conn), MQTT_SERVER);
+#endif
+    mqtt_set_inpub_callback(state->mqtt_client_inst, mqtt_incoming_publish_cb, mqtt_incoming_data_cb, state);
+    cyw43_arch_lwip_end();
+}
+
+// Call back com o resultado do DNS
+static void dns_found(const char *hostname, const ip_addr_t *ipaddr, void *arg) {
+    MQTT_CLIENT_DATA_T *state = (MQTT_CLIENT_DATA_T*)arg;
+    if (ipaddr) {
+        state->mqtt_server_address = *ipaddr;
+        start_client(state);
+    } else {
+        panic("dns request failed");
+    }
+}
